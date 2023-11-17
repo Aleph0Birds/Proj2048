f8fe687eaf51388e0ee1daa469530d9990b500e7
NoBirds@Desktop <wuqiufeng1417469@gmail.com>
2023-11-17 17:04:55 +0800

    minor constructer changes

--- Struct/Vector.py
+++ Struct/Vector.py
@@ -1,7 +1,18 @@
+from typing import overload
+
 class Vector:
-    def __init__(self, x: float | int, y: float | int = None):
+    @overload
+    def __new__(self, x: float | int, y: float | int):
+        ...
+
+    @overload
+    def __new__(self, value: float | int):
+        ...
+
+    def __new__(self, x: float | int, y: float | int | None = None):
         self.x = x 
         self.y = x if y is None else y
+        return self
 
-Vector.Zero: Vector = Vector(0)
-Vector.One: Vector = Vector(1)
+Vector.Zero: Vector = Vector(0, 0)
+Vector.One: Vector = Vector(1, 1)
