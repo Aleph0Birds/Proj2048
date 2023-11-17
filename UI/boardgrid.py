7537487d3c2d59d3f9068e037b93033975d029d0
NoBirds@Desktop <wuqiufeng1417469@gmail.com>
2023-11-17 17:42:41 +0800

    Initialize the board with 2 random "2" or "4"

--- UI/boardgrid.py
+++ UI/boardgrid.py
@@ -1,7 +1,9 @@
 import UI.window as win
+from UI.Color2048 import Color
 from Struct.Vector import Vector
 from Struct.Matrix import Matrix
 import tkinter as tk
+import tkinter.font as tkf
 
 padding = 36
 borderWidth = 10
@@ -18,21 +20,40 @@ class boardgrid:
         
         canvas.grid(row = 2, column=0, columnspan=4)
         self.grid = Matrix(4, 4)
-        self.initGrid(canvas)
+        self.initGridLayout(canvas)
 
         self.canvas = canvas
+        self.font = tkf.Font(canvas, size=30, family="Segoe UI Semibold")
 
-    def initGrid(self, canvas: tk.Canvas):
+    def initGridLayout(self, canvas: tk.Canvas):
         for i in range(self.grid.row):
             for j in range(self.grid.col):
-                r = canvas.create_rectangle(size.x * i + dp,
-                                            (size.y * j + dp),
-                                            size.x * (i + 1) + dp,
-                                            (size.y * (j + 1) + dp),
-                                            fill=win.BoardColor, outline=win.BorderColor,
-                                            width=borderWidth)
-                self.grid[i][j] = r
+                _ = self.__createRect(canvas, i, j, win.BoardColor)
 
     def updateGrid(self, dir: Vector):
         
         pass
+
+    def updateBoard(self, board: Matrix):
+        for i in range(board.row):
+            for j in range(board.col):
+                num = board[i][j]
+                if num != 0:
+                    color = Color.getColor(num)
+                    fcolor = "white" if num > 2 else "#7a6e66"
+                    self.grid[i][j] = (self.__createRect(self.canvas, i, j, color),
+                                        self.__createFont(self.canvas, i, j, str(pow(2, num)), fcolor))
+
+    def __createRect(self, canvas: tk.Canvas, i: int, j: int, bgcolor: str) -> int:
+        return canvas.create_rectangle(size.x * i + dp,
+                                        (size.y * j + dp),
+                                        size.x * (i + 1) + dp,
+                                        (size.y * (j + 1) + dp),
+                                        fill=bgcolor, outline=win.BorderColor,
+                                        width=borderWidth)
+    def __createFont(self, canvas: tk.Canvas, i: int, j: int, text: str, fgcolor: str) -> int:
+        return canvas.create_text(size.x * (i + .5) + dp,
+                                    (size.y * (j + .5) + dp),
+                                    font=self.font,
+                                    text=text,
+                                    fill=fgcolor)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from Struct.Matrix import Matrix
 
 def main():
-
     useUI()
+    initializeBoard()
+    win.mainloop()
 
 # range: 2^1 - 2^11+ -> 2 - 2048+
 def update() -> None:
@@ -10,19 +12,37 @@ def update() -> None:
         win.exit()
     # start working here
 
+### Task 10 ###
 def useUI():
     from UI.boardgrid import boardgrid 
     from UI.window import window
     from UI.indicator import indicator
     
     global win
-    global board
+    global boardGrid
     global indicate
     win = window()
-    board = boardgrid(win)
+    boardGrid = boardgrid(win)
     indicate = indicator(win)
     win.subscribeUpdate(update)
-    win.mainloop()
+
+def initializeBoard():
+    ### Task 1 ###
+    global board 
+    board = Matrix(4, 4)
+    ### Task 2 ###
+    from random import choice
+    from helper.utility import chance
+    n = 0
+    while n < 2:
+        r, c = choice(range(4)), choice(range(4))
+        if board[r][c] != 0:
+            continue
+        
+        board[r][c] = bool(chance(50)) + 1
+        n += 1
+        
+    boardGrid.updateBoard(board)
 
 if __name__ == "__main__":
     main()
