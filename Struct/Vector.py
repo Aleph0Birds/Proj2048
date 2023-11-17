6edf4a4e089552b758984ec8dfafcbcfab8f1098
NoBirds@Laptop <wuqiufeng1417469@gmail.com>
2023-11-17 10:41:56 +0800

    fixes logic error

--- Struct/Vector.py
+++ Struct/Vector.py
@@ -1,7 +1,7 @@
 class Vector:
     def __init__(self, x: float | int, y: float | int = None):
         self.x = x 
-        self.y = y if y is None else x
+        self.y = x if y is None else y
 
 Vector.Zero: Vector = Vector(0)
 Vector.One: Vector = Vector(1)
