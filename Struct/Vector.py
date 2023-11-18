f831b87481c1169773f737f4143c575e80fd8122
NoBirds@Desktop <wuqiufeng1417469@gmail.com>
2023-11-18 18:43:28 +0800

    adds some possibly helpful functions

--- Struct/Vector.py
+++ Struct/Vector.py
@@ -11,6 +11,23 @@ class Vector:
     
     def __repr__(self) -> str:
         return f"V({self.x}, {self.y})"
+    
+    def __iter__(self):
+        return iter((self.x, self.y))
+    
+    def __mul__(self, other):
+        if type(other) not in [int, float]: 
+            raise TypeError(f"other should be int or float, not {type(other)}")
+        
+        return Vector(self.x * other, self.y * other)
+    __rmul__ = __mul__
+
+    def __add__(self, other):
+        if type(other) != Vector: 
+            raise TypeError(f"other should be Vector, not {type(other)}")
+        
+        return Vector(self.x + other.x, self.y + other.y)
+    
 
 Zero = Vector(0)
 One = Vector(1)
