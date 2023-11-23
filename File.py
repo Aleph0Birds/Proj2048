253232f497a6a5bf702a7fb3a28f4cf2d4c6b33a
NoBirds@Desktop <wuqiufeng1417469@gmail.com>
2023-11-23 21:02:10 +0800

    marks the function as static cuz forgor💀

--- File.py
+++ File.py
@@ -9,7 +9,8 @@ class file:
         # if they want to cheat then let them cheat cuz they are not having fun
         with open(loc, "w") as f:
             f.write(str(score))
-            
+    
+    @staticmethod
     def readBestScore() -> int:
         if not Path(loc).exists(): return 0
         with open(loc, "r") as f:
